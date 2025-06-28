e01f4df7932b1c3ff1cff4655fb5b339917e830e
Yevgeni Shapiro <23049337+virtapp@users.noreply.github.com>
2025-06-28 23:16:56 +0300

    Create local.tf

---
+++ local.tf
@@ -0,0 +1,43 @@
+
+resource "kind_cluster" "default" {
+  name            = var.kind_cluster_name
+  kubeconfig_path = pathexpand(var.kind_cluster_config_path)
+  wait_for_ready  = true
+
+  kind_config {
+    kind        = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+
+    node {
+      role  = "control-plane"
+      image = "kindest/node:${var.k8s_version}"
+
+      kubeadm_config_patches = [
+        <<-EOT
+        kind: InitConfiguration
+        nodeRegistration:
+          kubeletExtraArgs:
+            node-labels: "ingress-ready=true"
+        EOT
+      ]
+
+      extra_port_mappings {
+        container_port = 80
+        host_port      = 80
+      }
+
+      extra_port_mappings {
+        container_port = 443
+        host_port      = 443
+      }
+    }
+
+    dynamic "node" {
+      for_each = toset(range(var.worker_count))
+      content {
+        role  = "worker"
+        image = "kindest/node:${var.k8s_version}"
+      }
+    }
+  }
+}
